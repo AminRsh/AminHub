8f714cd1876cc61b1d718d981850c1db58ab0254
AminRsh <amin.sharifi45@gmail.com>
2025-03-09 19:55:21 -0400

    Revert "callback_added"
    This reverts commit faa17c7d22cdbb6ee40c77820012f6305b552dc9.

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -3,7 +3,4 @@ import { fileRouter } from "./core";
 
 export const { GET, POST } = createRouteHandler({
   router: fileRouter,
-  config: {
-    callbackUrl: process.env.UPLOADTHING_CALLBACK_URL,
-  }
 });
